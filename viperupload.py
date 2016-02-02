de31f6d9411f80021fb4747fb1138907b01ee530
Nick Hoffman <securitykitten@users.noreply.github.com>
2016-02-01 21:32:15 -0500

    Update viperupload.py
    This add allows for tagging and submitting samples along with tags

--- viperupload.py
+++ viperupload.py
@@ -4,72 +4,66 @@ from os.path import isfile, join, abspath
 import hashlib
 import re
 import argparse
-from optparse import OptionParser
-
-'''
-This is a modified version of pmelson's upload script, with CLI support
-'''
+import pprint
 
 usage = "viperupload.py [-d DIRECTORY | -s SAMPLE] -t tags"
-opt_parser = OptionParser(usage=usage)
+parser = argparse.ArgumentParser(description=usage)
 
-opt_parser.add_option("-d", "--directory", action="store",dest="directory",default=None,
-  help="Directory to submit")
-opt_parser.add_option("-s", "--sample", action="store",dest="sample",default=None,
-  help="Single sample to submit")
-opt_parser.add_option("-t", "--tags", action="store",dest="tags",default=None,
-  help="Additional tags that the malware sample could use added")
-(options, args) = opt_parser.parse_args()
+parser.add_argument('-d','--directory',type=str, help='Directory to submit')
+parser.add_argument('-s','--sample',type=str,help='Single sample to submit')
+parser.add_argument('-t','--tags',type=str,help='Additional tags',nargs='+')
+args = parser.parse_args()
+dir = args.directory
+sample = args.sample
+tags = args.tags
 
-if not(bool(options.directory) ^ bool(options.sample)):
-   print 'Must provide either -d or -s options (not both)'
-   exit()
+if not(bool(dir)^bool(sample)):
+	print 'Must provide either -s or -d options (not both)'
+	exit()
+	
+def add_tags(filesha,tags):
+	#this method will add all tags that should be added by default
+	#imphash will be added in addition to user specified tags
+	url_tag = 'http://localhost:8080/file/tags/add'
+	url_run = 'http://localhost:8080/modules/run'
+	#imphash method
+	params = {'sha256': filesha, 'cmdline': 'pe imphash'}
+	r = requests.post(url_run,params)
+	data = r.json()
+	searchobj = re.search(r'Imphash\:\ \\x1b\[1m(.+?)\\x1b\[0m', data)
+	if not searchobj is None:
+		tags.append(searchobj.group(1))
+	params = {'sha256': filesha, 'tags': ",".join(tags)}
+	r = requests.post(url_tag, params)
 
-url_upload = 'http://localhost:8080/file/add'
-url_tag = 'http://localhost:8080/file/tags/add'
-url_run = 'http://localhost:8080/modules/run'
+def submit_sample(fullpath,tags):
+	url_upload = 'http://localhost:8080/file/add'
+	url_search = 'http://localhost:8080/file/find'
+	#do a quick search for the file so we don't add it multiple times
+	#viper will de-dup multiple uploads, but this is unnecessary and 
+	#could potentially cause overhead in the case of large upload sets
+	filesha = hashlib.sha256(open(fullpath,'rb').read()).hexdigest()
+	search = {'sha256': filesha}
+	r = requests.post(url_search, search)
+	data = r.json()
+	if(len(data) > 0):
+		#our sample is already in the DB, we'll just add the tags
+		print("%s is already in the DB" % fullpath)
+		add_tags(filesha,tags)
+	else:
+		#in this case, we add the sample and the tags
+		print("%s added to DB")
+		files = {'file': open(file, 'rb')}
+		r = requests.post(url_upload, files=files)
+		add_tags(filesha,tags)
+		
+if (dir):
+	filelist = [ f for f in listdir(dir) if isfile(join(dir,f)) ]
+	for file in filelist:
+		fullpath = join(dir,file)
+		submit_sample(fullpath,tags)
 
-if (options.directory):
-  #expand the directory and store as filelist
-  filelist = [ f for f in listdir(options.directory) if isfile(join(options.directory,f)) ]
-  for file in filelist:
-    fullpath = join(options.directory,file)
-    files = {'file': open(fullpath, 'rb')}
-    #upload the file
-    r = requests.post(url_upload, files=files)
-    filesha = hashlib.sha256(open(fullpath,'rb').read()).hexdigest()
-    params = {'sha256': filesha, 'cmdline': 'pe imphash'}
-    #send the command
-    r = requests.post(url_run,params)
-    data = r.json()
-    searchobj = re.search(r'Imphash\:\ \\x1b\[1m(.+?)\\x1b\[0m', data)
-    imphash = searchobj.group(1) 
-    print imphash
-    if (options.tags is None):
-      params = {'sha256': filesha, 'tags': imphash}
-    else:
-      params = {'sha256': filesha, 'tags': imphash + "," + options.tags}
-    #send the request
-    r = requests.post(url_tag, params)
-
-if (options.sample):
-  #for an individual sample,  i'll keep a lot the same logic as above
-  if (isfile(options.sample)):
-    fullpath = abspath(options.sample)
-    files = {'file': open(fullpath, 'rb')}
-    #upload the file
-    r = requests.post(url_upload, files=files)
-    filesha = hashlib.sha256(open(join(filepath,file)).read()).hexdigest()
-    params = {'sha256': filesha, 'cmdline': 'pe impash'}
-    #send the command
-    r = requests.post(url_run,params)
-    data = r.json()
-    searchobj = re.search(r'Imphash\:\ \\x1b\[1m(.+?)\\x1b\[0m', data)
-    imphash = searchobj.group(1)
-    print imphash
-    if (options.tags is None):
-      params = {'sha256': filesha, 'tags': imphash}
-    else:
-      params = {'sha256': filesha, 'tags': imphash + "," + options.tags}
-    #send the request
-    r = requests.post(url_tag, params)
+if (sample):
+	if (isfile(sample)):
+		fullpath = abspath(sample)
+		submit_sample(fullpath,tags)
